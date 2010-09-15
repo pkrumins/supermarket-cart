6eeef0ab8bb910ca386795f51e113567c0afbcbb
James Halliday <substack@gmail.com>
2010-09-14 22:56:59 -0800

    web connect test that fails

---
+++ test/web.js
@@ -0,0 +1,36 @@
+var Cart = require('cart');
+var connect = require('connect');
+var http = require('http');
+
+exports.webserver = function (assert) {
+    var port = Math.floor(Math.random() * 40000 + 10000);
+    
+    var server = connect.createServer();
+    server.use(connect.cookieDecoder());
+    //*
+    server.use(connect.session({
+        store : new Cart({ dbFile : '/tmp/cart-' + port + '.db' }),
+        secret : 'meowmers',
+    }));
+    //*/
+    server.use(connect.router(function (app) {
+        app.get('/', function (req, res) {
+            res.writeHead(200, { 'Content-Type' : 'text/html' });
+            res.write('catface');
+            res.end();
+        });
+    }));
+    server.listen(port, 'localhost');
+    
+    {
+        var client = http.createClient(port);
+        var req = client.request('GET', '/', { host : 'localhost' });
+        req.end();
+        req.on('response', function (res) {
+            res.on('data', function (data) {
+                assert.equal('catface', data.toString());
+            });
+            server.close();
+        });
+    }
+};
